e4c92ed9b5041bb790054cf9c374f740ffa37451
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-03-17 18:19:35 -0400

    Added methods

---
+++ WriteToFile.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class WriteToFile {
+
+    public void writeToCSV() {
+    }
+
+    public void writeToJSON() {
+    }
+
+    public String[] readFromCSV()  {
+        FileWriter writer = new FileWriter();
+        return null;
+
+    }
+
+    public String[] readFromJSON() {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
