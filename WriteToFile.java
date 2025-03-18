94db8dcf1bae56bf08fc0009aacada122ea8dac6
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-03-18 19:17:17 +0000

    Added readFromCSV method

--- WriteToFile.java
+++ WriteToFile.java
@@ -1,4 +1,13 @@
+/**
+ * This class represents reading and writing to both JSON and CSV files
+ *
+ * @author Abdul Hamdan, Shahob Zekria
+ * @version 1.0
+ */
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteToFile {
 
@@ -8,10 +17,36 @@ public class WriteToFile {
     public void writeToJSON() {
     }
 
-    public String[] readFromCSV()  {
-        FileWriter writer = new FileWriter();
-        return null;
 
+    /**
+     * Function reads from CSV file and returns a list of rows,
+     *
+     * @return dataList
+     */
+
+    public List<String[]> readFromCSV(String csv_file) throws IOException {
+        List<String[]> dataList = new ArrayList<>();
+        String line;
+        // Define the delimiter used to separate the values in the CSV (comma in this case)
+        String delimiter = ",";
+        try (BufferedReader br = new BufferedReader(new FileReader(csv_file))) {
+            br.readLine();
+            // Read each  line of the CSV file until end of file
+            while ((line = br.readLine()) != null) {
+            // Split the line into an array of strings using the delimiter
+                String[] data = line.split(delimiter);
+                dataList.add(data);   // Add the split data (which is equivalent to one row) to the dataList
+                for (String value : data) {
+                    System.out.print(value + "\t"); // testing output
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle any file reading errors
+        }
+
+        return dataList;
     }
 
     public String[] readFromJSON() {
